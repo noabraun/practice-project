9e9d4d2cbb6ccf8d9221615c59de8d3f7901ecce
Hackbright Student <no-reply@hackbrightacademy.com>
2017-09-18 16:30:40 -0700

    seed.py ingests data files

---
+++ model.py
@@ -0,0 +1,86 @@
+"""Models and database functions for Ratings project."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# This is the connection to the PostgreSQL database; we're getting this through
+# the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+
+##############################################################################
+# Model definitions
+
+class User(db.Model):
+    """User of ratings website."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String(64), nullable=True)
+    password = db.Column(db.String(64), nullable=True)
+    age = db.Column(db.Integer, nullable=True)
+    zipcode = db.Column(db.String(15), nullable=True)
+
+    def __repr__(self):
+        """Show info about user"""
+
+        return "<User user_id={} email={} password={} age={} zipcode={}>".format(
+               self.user_id, self.email, self.password, self.age, self.zipcode)
+
+
+# Put your Movie and Rating model classes here.
+class Movie(db.Model):
+    """Movies on ratings webstite."""
+
+    __tablename__ = "movies"
+
+    movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String(256), nullable=True)
+    released_at = db.Column(db.DateTime, nullable=True)
+    imdb_url = db.Column(db.String(256), nullable=True)
+
+    def __repr__(self):
+        """Show info about movie"""
+
+        return "<User movie_id={} title={} released_at={} imdb_url={}>".format(
+               self.movie_id, self.title, self.released_at, self.imdb_url)
+
+
+class Rating(db.Model):
+    """ User ratings for movies """
+
+    __tablename__ = 'ratings'
+
+    rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    movie_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        """Show info about rating"""
+
+        return "<User rating_id={} movie_id={} user_id={} score={}>".format(
+               self.rating_id, self.movie_id, self.user_id, self.score)
+
+##############################################################################
+# Helper functions
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///ratings'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print "Connected to DB."
---
+++ seed.py
@@ -0,0 +1,113 @@
+"""Utility file to seed ratings database from MovieLens data in seed_data/"""
+
+from sqlalchemy import func
+from model import User, Rating, Movie
+
+from model import connect_to_db, db
+from server import app
+
+from datetime import datetime
+
+
+def load_users():
+    """Load users from u.user into database."""
+
+    print "Users"
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    User.query.delete()
+
+    # Read u.user file and insert data
+    for row in open("seed_data/u.user"):
+        row = row.rstrip()
+        user_id, age, gender, occupation, zipcode = row.split("|")
+
+        user = User(user_id=user_id,
+                    age=age,
+                    zipcode=zipcode)
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(user)
+
+    # Once we're done, we should commit our work
+    db.session.commit()
+
+
+def load_movies():
+    """Load movies from u.item into database."""
+
+    print "Movies"
+
+    Movie.query.delete()
+
+    for row in open("seed_data/u.item"):
+        row = row.rstrip().split('|')
+
+        if row[2]:
+            date = datetime.strptime(row[2], "%d-%b-%Y")
+        else:
+            date = None
+
+        if len(row[1]) > 7:
+            title = row[1][:-7]
+        else:
+            title = row[1]
+
+        if row[4]:
+            url = row[4]
+        else:
+            url = None
+        #take out if else
+
+        movie = Movie(movie_id=row[0],
+                      title=title,
+                      released_at=date,
+                      imdb_url=url)
+
+        db.session.add(movie)
+
+    db.session.commit()
+
+
+def load_ratings():
+    """Load ratings from u.data into database."""
+
+    print "Ratings"
+
+    Rating.query.delete()
+
+    for row in open('seed_data/u.data'):
+        row = row.rstrip().split('\t')
+
+        rating = Rating(movie_id=row[1], user_id=row[0], score=row[2])
+
+        db.session.add(rating)
+
+    db.session.commit()
+
+
+def set_val_user_id():
+    """Set value for the next user_id after seeding database"""
+
+    # Get the Max user_id in the database
+    result = db.session.query(func.max(User.user_id)).one()
+    max_id = int(result[0])
+
+    # Set the value for the next user_id to be max_id + 1
+    query = "SELECT setval('users_user_id_seq', :new_id)"
+    db.session.execute(query, {'new_id': max_id + 1})
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    # In case tables haven't been created, create them
+    db.create_all()
+
+    # Import different types of data
+    load_users()
+    load_movies()
+    load_ratings()
+    set_val_user_id()
